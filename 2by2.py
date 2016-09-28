4df8d1283c5478a74cc4f3590c8af2d82ad12a5c
JCharante <jcharante@gmail.com>
2016-09-28 10:45:22 -0400

    Added gitignore & cube class

---
+++ 2by2.py
@@ -0,0 +1,11 @@
+class Cube:
+
+    def __init__(self):
+        faces = {
+            'u': [[None, None], [None, None]],
+            'd': [[None, None], [None, None]],
+            'l': [[None, None], [None, None]],
+            'r': [[None, None], [None, None]],
+            'f': [[None, None], [None, None]],
+            'b': [[None, None], [None, None]]
+        }
