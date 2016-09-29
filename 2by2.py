01113ec0bef5acb7594327938324f292fb72fba3
JCharante <jcharante@gmail.com>
2016-09-29 10:48:31 -0400

    Added test suite

--- 2by2.py
+++ 2by2.py
@@ -1,3 +1,5 @@
+import json
+
 # Colors
 white = 'white'
 green = 'green'
@@ -29,24 +31,37 @@ class Cube:
                 output += '{} '.format(cell)
         return output
 
+    def dump(self):
+        return json.dumps(self.faces)
+
+    def load(self, json_string):
+        self.faces = json.loads(json_string)
+
     def turn_u(self):
         # Change Top
         self.faces['u'][0][1], self.faces['u'][1][1], self.faces['u'][0][0], self.faces['u'][1][0] = self.faces['u'][0][0], self.faces['u'][0][1], self.faces['u'][1][0], self.faces['u'][1][1]
         # Changing other sides
         self.faces['l'][0][0], self.faces['l'][0][1], self.faces['b'][0][0], self.faces['b'][0][1], self.faces['f'][0][0], self.faces['f'][0][1], self.faces['r'][0][0], self.faces['r'][0][1] = self.faces['f'][0][0], self.faces['f'][0][1], self.faces['l'][0][0], self.faces['l'][0][1], self.faces['r'][0][0], self.faces['r'][0][1], self.faces['b'][0][0], self.faces['b'][0][1]
 
-cube = Cube()
-print(cube.face_to_str('u'))
-print(cube.face_to_str('d'))
-print(cube.face_to_str('l'))
-print(cube.face_to_str('r'))
-print(cube.face_to_str('f'))
-print(cube.face_to_str('b'))
-cube.turn_u()
-print("Turned")
-print(cube.face_to_str('u'))
-print(cube.face_to_str('d'))
-print(cube.face_to_str('l'))
-print(cube.face_to_str('r'))
-print(cube.face_to_str('f'))
-print(cube.face_to_str('b'))
+
+class TestSuite:
+    def __init__(self):
+        pass
+
+    def turn_u(self):
+        test_cube = Cube()
+        before = '{"f": [["white", "white"], ["white", "white"]], "r": [["blue", "blue"], ["blue", "blue"]], "l": [["green", "green"], ["green", "green"]], "b": [["yellow", "yellow"], ["yellow", "yellow"]], "d": [["red", "red"], ["red", "red"]], "u": [["orange", "orange"], ["orange", "orange"]]}'
+        expected_after = json.loads('{"f": [["blue", "blue"], ["white", "white"]], "r": [["yellow", "yellow"], ["blue", "blue"]], "l": [["white", "white"], ["green", "green"]], "b": [["green", "green"], ["yellow", "yellow"]], "d": [["red", "red"], ["red", "red"]], "u": [["orange", "orange"], ["orange", "orange"]]}')
+        test_cube.load(before)
+        test_cube.turn_u()
+        after = dict(test_cube.faces)
+        return expected_after == after
+
+    def run_all_tests(self):
+        if self.turn_u():
+            print("Turn U Test Passed")
+        else:
+            print("Turn U Test Failed")
+
+test = TestSuite()
+test.run_all_tests()
